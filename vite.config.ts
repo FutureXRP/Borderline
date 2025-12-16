aab166aeb1cf5e727681b48d965314fe53c69dcf
FutureXRP <the5blairsworld@gmail.com>
2025-12-16 12:24:52 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Correct base path for your repo
 export default defineConfig({
   plugins: [react()],
-  base: '/borderline-orders-and-supply/',
+  base: '/Borderline/',
 })
